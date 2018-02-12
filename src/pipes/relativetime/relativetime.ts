f5dc1fc79feb4f0bf7f9cd4ffbfe54ab686cdf80
jiaen188 <244189981@qq.com>
2018-02-12 21:20:13 +0800

    add platform browser

--- src/pipes/relativetime/relativetime.ts
+++ src/pipes/relativetime/relativetime.ts
@@ -13,6 +13,15 @@ export class RelativetimePipe implements PipeTransform {
   /**
    * Takes a value and makes it lowercase.
    */
+
+  /**
+   * 将日期格式转化成对应时间格式
+   * 
+   * @param {string} value 
+   * @param {any} args 
+   * @returns 
+   * @memberof RelativetimePipe
+   */
   transform(value: string, ...args) {
     return moment(value).toNow();
   }
